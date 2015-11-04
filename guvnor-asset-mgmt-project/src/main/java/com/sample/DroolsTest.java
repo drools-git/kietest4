9bc60199462a154f0d2d1e9ef4527b1b2f30203a
drools-git <inhumanvision@gmail.com>
2015-11-04 18:00:46 -0430

    f

---
+++ guvnor-asset-mgmt-project/src/main/java/com/sample/DroolsTest.java
@@ -0,0 +1,39 @@
+package com.sample;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+/**
+ * This is a sample class to launch a rule.
+ */
+
+
+      public class Message {
+
+        public static final int HELLO = 0;
+        public static final int GOODBYE = 1;
+
+        private String message;
+
+        private int status;
+
+        public String getMessage() {
+            return this.message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public int getStatus() {
+            return this.status;
+        }
+
+        public void setStatus(int status) {
+            this.status = status;
+        }
+
+    }
+
+
